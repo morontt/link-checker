d56e67f86620960c95e5dbf7c5613f4920763f24
morontt <morontt@gmail.com>
2016-03-08 01:49:11 +0200

    hardwork

--- src/main/java/info/morontt/link_checker/App.java
+++ src/main/java/info/morontt/link_checker/App.java
@@ -1,12 +1,32 @@
 package info.morontt.link_checker;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.yaml.snakeyaml.Yaml;
+
 /**
- * Hello world!
+ * Spider link-checker
  */
-public class App
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+public class App {
+    public static void main(String[] args) throws IOException {
+        System.out.println("");
+        System.out.println("#");
+        System.out.println("#           #    #    #  #    #           ####   #    #  ######   ####   #    #");
+        System.out.println("#           #    ##   #  #   #           #    #  #    #  #       #    #  #   #");
+        System.out.println("#           #    # #  #  ####    #####   #       ######  #####   #       ####");
+        System.out.println("#           #    #  # #  #  #            #       #    #  #       #       #  #");
+        System.out.println("#           #    #   ##  #   #           #    #  #    #  #       #    #  #   #");
+        System.out.println("#######     #    #    #  #    #           ####   #    #  ######   ####   #    #");
+        System.out.println("");
+
+        InputStream input = new FileInputStream(new File("./config.yaml"));
+        Yaml yaml = new Yaml();
+        Object data = yaml.load(input);
+        input.close();
+
+        System.out.println(data);
     }
 }
